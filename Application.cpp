7a1ceb5f717ccf307366d76f7ee7b3a5f5b94cb1
Hemnath Balasubramanian <hemnath@thepenindia.com>
2025-11-10 01:52:13 +0530

    added color to window

--- Application.cpp
+++ Application.cpp
@@ -32,6 +32,12 @@ int main() {
 
         while (!glfwWindowShouldClose(window)) {
             processInput(window);
+
+            // render
+            // ------
+            glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
             glfwSwapBuffers(window);
             glfwPollEvents();
         }
